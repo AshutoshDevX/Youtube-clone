68eb4ab76b102ada7ee63d04e75ee5c4483eded6
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-04-07 03:15:24 +0530

    Add VideoSuggestions

---
+++ frontend/src/components/VideoSuggestion.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { VideoSuggestionCard } from './VideoSuggestionCard'
+
+export const VideoSuggestion = () => {
+    return (
+        <div className="space-y-2">
+            <VideoSuggestionCard />
+            <VideoSuggestionCard />
+            <VideoSuggestionCard />
+            <VideoSuggestionCard />
+            <VideoSuggestionCard />
+            <VideoSuggestionCard />
+            <VideoSuggestionCard />
+            <VideoSuggestionCard />
+        </div>
+    )
+}
