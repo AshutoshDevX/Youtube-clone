505154d7e30e869f9fb9a0a96adcfdb3200899c2
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-04-08 16:44:35 +0530

    connect database

---
+++ backend/db/connect.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const db = mongoose.connection
+
+db.on("connected", () => {
+    console.log("database connected successfully")
+})
+db.on('disconnected', () => {
+    console.log("database connection disconnected")
+})
+
+try {
+    await mongoose.connect("mongodb+srv://admin:Ashutosh123@cluster1.27bkq.mongodb.net/");
+}
+catch (err) {
+    console.log(err);
+}
+
