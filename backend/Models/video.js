1defe1aef444d87273d143c5f14fadcfb52e8d3d
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-04-08 16:45:20 +0530

    add video model

---
+++ backend/Models/video.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+
+const video = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    videoUrl: {
+        type: String,
+        required: true
+    },
+    thumbnailUrl: {
+        type: String,
+        required: true
+    },
+    tags: {
+        type: [String],
+        trim: true,
+        default: "All",
+        lowercase: true
+    },
+    like: {
+        type: Number,
+        default: 0
+    },
+    dislike: {
+        type: Number,
+        default: 0
+    }
+})
+
+
+const Video = mongoose.model("Video", video);
+
+export default Video
