ef00086fff770143b35d73ae905e5fc45e59cee7
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-04-10 00:04:38 +0530

    udpate cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ import commentRouter from './Routes/comment.js'
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(cookieParser());
 
 app.use('/api/user', userRouter);
