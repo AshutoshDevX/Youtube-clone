eaece555e67e5c77672bcff576ef497e6ac7eaf5
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-04-10 02:32:40 +0530

    fetched data from backend

--- frontend/src/components/VideoSection.jsx
+++ frontend/src/components/VideoSection.jsx
@@ -1,8 +1,21 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { VideoSectionCard } from './VideoSectionCard'
 import { ToggleContext } from '../App'
 import { Link } from 'react-router'
+import axios from 'axios'
+
+
 export const VideoSection = () => {
+    const [videoData, setVideoData] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/video").then((response) => {
+            setVideoData(response.data.allVideos);
+        }).catch((err) => {
+            console.log(err)
+        })
+    }, [])
+    console.log(videoData)
     const [toggle] = useContext(ToggleContext);
     const filters = ["All", "APIs", "Computer Science", "Podcasts", "Music", "Gaming", "Mixes", "Live", "Recruitment", "News", "Stocks", "Ideas", "Property", "Cricket", "Comedy", "Recently Uploaded", "Watched", "Near to you"]
     return (
@@ -15,23 +28,12 @@ export const VideoSection = () => {
                 </div>
             </div>
             <div className={toggle ? "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-5 gap-5 md:scrollbar lg:scrollbar scrollbar-none" : "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 p-5 gap-5 md:scrollbar lg:scrollbar scrollbar-none"}>
-                <Link to="/watch"><VideoSectionCard /></Link>
-                <VideoSectionCard />
-                <VideoSectionCard />
-                <VideoSectionCard />
-                <VideoSectionCard />
-                <VideoSectionCard />
-                <VideoSectionCard />
-                <VideoSectionCard />
-                <VideoSectionCard />
-                <VideoSectionCard />
-                <VideoSectionCard />
-                <VideoSectionCard />
-                <VideoSectionCard />
-                <VideoSectionCard />
-                <VideoSectionCard />
-                <VideoSectionCard />
-                <VideoSectionCard />
+                {
+                    videoData.map(({ thumbnailUrl, title, userId, createdAt, views, videoUrl }) => {
+                        return <Link to="/watch"><VideoSectionCard thumbnailUrl={thumbnailUrl} title={title} userId={userId} createdAt={createdAt} views={views} videoUrl={videoUrl} /></Link>
+                    })
+                }
+
             </div>
         </div>
     )
