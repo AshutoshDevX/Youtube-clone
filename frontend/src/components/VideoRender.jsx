f1781d2a01c08285e61c8d2e9bf4d1e6b70a5daa
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-04-10 06:20:24 +0530

    rendered fetched data

--- frontend/src/components/VideoRender.jsx
+++ frontend/src/components/VideoRender.jsx
@@ -1,28 +1,31 @@
 import React, { useState } from 'react'
-import banner from '../assets/banner.png'
 import { AiOutlineLike, AiOutlineDislike } from 'react-icons/ai';
 import { PiShareFatBold } from 'react-icons/pi';
 import { BiDownload } from 'react-icons/bi';
 import { BsThreeDots } from 'react-icons/bs';
-
-export const VideoRender = () => {
+import { relativeTime } from '../utils/util';
+import { Link } from 'react-router';
+export const VideoRender = ({ createdAt, videoUrl, userId, description, title, like, dislike, views }) => {
+    const relativePeriod = relativeTime(createdAt);
     const [isActive, setIsActive] = useState(false);
     return (
         <div className=" w-full h-auto flex flex-col gap-2">
             <video width="400" className="w-full rounded-xl" controls autoPlay>
-                <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4" />
+                <source src={videoUrl} type="video/mp4" />
                 Your Browser does not support the video tag
             </video>
             <div className="text-lg font-bold">
-                <p>Build Youtube Clone Using MERN Full Stack | Build Complete Website Like YouTube In MERN 2024</p>
+                <p>{title}</p>
             </div>
             <div className="flex lg:flex-row md:flex-col flex-col lg:justify-between gap-3">
                 <div className="flex gap-2 items-center">
-                    <div className="lg:h-10 lg:w-10 md:h-9 md:w-9 h-8 w-8">
-                        <img className="h-full w-full rounded-full" src={banner} />
-                    </div>
+                    <Link to={`/channel/${userId._id}`}>
+                        <div className="lg:h-10 lg:w-10 md:h-9 md:w-9 h-8 w-8">
+                            <img className="h-full w-full rounded-full" src={userId.profileUrl} />
+                        </div>
+                    </Link>
                     <div className="mr-4">
-                        <p className="font-bold">Coding Guru</p>
+                        <p className="font-bold">{userId.channelName}</p>
                         <p>361k Subscribers</p>
                     </div>
                     <div className="bg-black px-3 py-2 h-fit text-white text-md font-semibold rounded-full flex justify-center items-center">
@@ -33,7 +36,7 @@ export const VideoRender = () => {
 
                     <div className="px-3 py-2 h-fit text-white text-md font-semibold rounded-full flex justify-center items-center gap-2 bg-stone-100">
                         <AiOutlineLike className="text-black" />
-                        <span className="text-black">4.1K</span>
+                        <span className="text-black">{like}</span>
                         <span className="text-stone-400">|</span>
                         <AiOutlineDislike className="text-black" />
                     </div>
@@ -58,11 +61,11 @@ export const VideoRender = () => {
             <div onClick={() => setIsActive(!isActive)}
                 className={isActive ? "bg-stone-100 p-3 h-auto rounded-lg " : "bg-stone-100 p-3 h-24 rounded-lg  overflow-hidden"} >
                 <div className="flex gap-2 font-semibold">
-                    <p>76k views</p>
-                    <p>2 weeks ago</p>
+                    <p>{views} views</p>
+                    <p></p>
                 </div>
                 <div className={isActive ? "h-auto" : "h-12 overflow-hidden"}>
-                    Lorem ipsum, Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla nesciunt sit ullam nostrum porro vitae, ad reiciendis est ut doloribus. Sapiente, nostrum? Expedita nobis incidunt repellat officia, possimus blanditiis recusandae.dolor sit amet consectetur adipisicing elit. Facilis accusamus soluta nobis illum. Accusantium voluptates, vitae inventore, ex voluptatum provident ducimus perspiciatis ipsa qui vel debitis. Dolor unde quas molestiae.
+                    {description}
                 </div>
             </div>
         </div>
