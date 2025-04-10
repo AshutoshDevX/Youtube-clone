7b5f9c693523deed392460ba1e607280361553ab
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-04-10 18:53:04 +0530

    fetched data from backend

--- frontend/src/pages/Channel.jsx
+++ frontend/src/pages/Channel.jsx
@@ -4,12 +4,14 @@ import { ChannelInfo } from '../components/ChannelInfo'
 import { MiniSideBar } from '../components/MiniSideBar'
 import { ToggleContext } from '../App'
 import { useContext } from 'react'
+import { useParams } from 'react-router-dom'
 export const Channel = () => {
+    const { id } = useParams();
     const [toggle] = useContext(ToggleContext)
     return (
         <div className="bg-white flex">
             {toggle ? <Sidebar /> : <MiniSideBar />}
-            <ChannelInfo />
+            <ChannelInfo id={id} />
         </div>
 
     )
