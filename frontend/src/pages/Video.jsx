8ad9ea3e69f1c0e78686418f6a140bec2258346a
AshutoshDevX <ashutoshbhagat74@gmail.com>
2025-04-10 06:18:12 +0530

    fetched data from backend

--- frontend/src/pages/Video.jsx
+++ frontend/src/pages/Video.jsx
@@ -1,27 +1,42 @@
-import React from 'react'
-import { VideoRender } from '../components/VideoRender'
-import { Comments } from '../components/Comments'
-import { VideoSuggestion } from '../components/VideoSuggestion'
+import React, { useEffect, useState } from 'react';
+import { VideoRender } from '../components/VideoRender';
+import { Comments } from '../components/Comments';
+import { VideoSuggestion } from '../components/VideoSuggestion';
+import axios from 'axios';
+import { useParams } from 'react-router';
 
 export const Video = () => {
     const filters = ["All", "Related", "For you", "Recently uploaded", "Watched", "Programming", "Javascript"]
-    return (
+    const { id } = useParams();
+    const [videoData, setVideoData] = useState(null);
 
-        <div className="py-6 lg:px-20 lg:flex gap-6 md:px-7 px-4">
-            <div className="lg:w-[70%] md:w-full w-full space-y-3">
-                <VideoRender />
-                <Comments />
-            </div>
-            <div className="lg:w-[30%] space-y-5 md:w-full w-full">
-                <div className="bg-white">
-                    <div className="flex gap-3 py-2 w-[100vw-16px]  overflow-x-scroll scrollbar-none">
-                        {filters.map((item) => {
-                            return <p className="whitespace-nowrap px-3 py-1 w-fit h-fit rounded-md text-sm font-semibold bg-stone-100">{item}</p>
-                        })}
-                    </div>
+    useEffect(() => {
+        axios.get(`http://localhost:3000/api/video/${id}`)
+            .then((response) => {
+                setVideoData(response.data.video);
+            }).catch((err) => {
+                console.log(err);
+            })
+    }, [])
+
+    return (
+        <>
+            {videoData && <div className="py-6 lg:px-20 lg:flex gap-6 md:px-7 px-4">
+                <div className="lg:w-[70%] md:w-full w-full space-y-3">
+                    {videoData && <VideoRender videoUrl={videoData.videoUrl} userId={videoData.userId} description={videoData.description} title={videoData.title} like={videoData.like} createdAt={videoData.createdAt} dislike={videoData.dislike} views={videoData.views} />}
+                    <Comments videoId={videoData._id} />
                 </div>
-                <VideoSuggestion />
-            </div>
-        </div>
+                <div className="lg:w-[30%] space-y-5 md:w-full w-full">
+                    <div className="bg-white">
+                        <div className="flex gap-3 py-2 w-[100vw-16px]  overflow-x-scroll scrollbar-none">
+                            {filters.map((item) => {
+                                return <p className="whitespace-nowrap px-3 py-1 w-fit h-fit rounded-md text-sm font-semibold bg-stone-100">{item}</p>
+                            })}
+                        </div>
+                    </div>
+                    <VideoSuggestion thumbnailUrl={videoData.thumbnailUrl} title={videoData.title} views={videoData.views} createdAt={videoData.createdAt} />
+                </div>
+            </div>}
+        </>
     )
 }
